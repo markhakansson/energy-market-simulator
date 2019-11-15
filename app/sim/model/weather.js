ad1a2e663732f4bfa1e77786d0529c017fc1d822
Aron <arostr-5@student.ltu.se>
2019-11-15 15:03:30 +0100

    removed wind direction and description

--- app/sim/model/weather.js
+++ app/sim/model/weather.js
@@ -3,26 +3,15 @@
 class Weather {
     constructor() {
         this.wind_speed = 0;
-        this.wind_direction = "";
-        this.description = "";
+        
     }
 
-    setWeather(wind_speed, wind_direction, description) {
+    setWeather(wind_speed) {
         this.wind_speed = wind_speed;
-        this.wind_direction = wind_direction;
-        this.description = description;
     }
 
     getWindSpeed() {
         return this.wind_speed;
-    }
-
-    getWindDir() {
-        return this.wind_direction;
-    }
-
-    getDescription() {
-        return this.description;
     }
 
 }
