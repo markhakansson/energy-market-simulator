594e955854919c3a2346416cc4aab06c1dda31f6
Mark Hakansson <mark.hakansson@live.se>
2019-11-18 15:36:59 +0100

    Add generation of electricity consumption in consumer model in sim

--- app/sim/model/consumer.js
+++ app/sim/model/consumer.js
@@ -1,15 +1,28 @@
-
+var gauss = require('../../helper/gauss')
 
 class Consumer {
     constructor(name, market, time) {
         this.name = name;
         this.market = market;
         this.time = time;
-        this.consumption = 0;
+        this.consumption = 3000;
     }
 
-    setConsumption() {
-        this.consumption = 10; // should be gauss distribution
+    setConsumption(consumption) {
+        this.consumption = consumption; // should be gauss distribution
+    }
+
+    generateConsumption() {
+        // Yearly consumption 25,000 kWh around 70 kWh a day/ 3 kWh an hour
+        var consumption = this.consumption / 1000;
+        var arr;
+        // Threshold of 4 kWh. If it reaches over that point the distribution will favor smaller wind speeds.
+        if(consumption < 4.0) {
+            arr = [0.8 * consumption, consumption, 1.2 * consumption];
+        } else {
+            arr = [0.8 * consumption, 0.9 * consumption, 0.95 * consumption, 1.1 * consumption];
+        }
+        this.consumption = gauss(arr, 4, 0.1) * 1000;   
     }
 
     getConsumption() {
