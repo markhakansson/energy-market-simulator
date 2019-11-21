603fafd0909d6c3141cec2c7e92c92fb17041bc2
Mark Hakansson <mark.hakansson@live.se>
2019-11-21 09:35:24 +0100

    Put time multiplier in the class

--- app/sim/model/consumer.js
+++ app/sim/model/consumer.js
@@ -2,13 +2,14 @@ var gauss = require('../../helper/gauss')
 var tools = require('../../helper/tools');
 
 class Consumer {
-    constructor(name, market, time) {
+    constructor(name, market) {
         this.name = name;
         this.market = market;
         this.consumption = 3000; // in Wh
         this.bought = 0;
+        this.timeMultiplier = 5;
         this.blackout = false;
-        this.retry = false;
+        this.retrying = false;
     }
 
     setConsumption(consumption) {
@@ -32,23 +33,21 @@ class Consumer {
             this.buyFromMarket(consumption);
         
         // If blackout has occured, try to buy from market in the future
-        } else if(this.blackout || !this.retry) {
-            this.retry = true;
+        } else if(this.blackout && !this.retrying) {
+            this.retrying = true;
 
-            tools.sleep(10 * 1000).then(() => {
+            tools.sleep(2 * this.timeMultiplier * 1000).then(() => {
                 arr = [0.8 * 3, 3, 1.2 * 3];
                 consumption = gauss.gauss(arr, 4, 0.05) * 1000;
-                console.log("DEBUG: Consumption is " + consumption);
                 this.buyFromMarket(consumption);
+                this.retrying = this.blackout;
             });
         }
     }
 
     buyFromMarket(energy) {
-        console.log("DEBUG: Buying " + energy + " Wh");
         let boughtEnergy = this.market.buy(energy); 
         this.bought = boughtEnergy;
-        console.log("DEBUG: Bought "+ boughtEnergy + " Wh");
         
         if(boughtEnergy == 0) {
             this.consumption = 0;
@@ -70,7 +69,7 @@ class Consumer {
         "\n Consuming: " + this.consumption + " Wh" + 
         "\n Bought energy: " + this.bought + " Wh" +
         "\n Price per Wh is: " + this.market.price +" SEK" +
-        "\n Black out: " + this.blackout
+        "\n Blackout: " + this.blackout
         );
 
     }
