36b1f186a71c7d7020fab4dee1afea6146b9d0fa
Aron <arostr-5@student.ltu.se>
2019-11-28 10:54:52 +0100

    updated timereport and implement a test for prosumer

--- app/sim/tests/prosumer.test.js
+++ app/sim/tests/prosumer.test.js
@@ -5,9 +5,13 @@ const ProsumerSim = require('../model/prosumer');
 describe('#randomizeTurbineBreaking', function() {
     it("Turbine should break", () => {
         // name, market, wind, fillBattRatio, useBattRatio, batterySize
-        let ProsumerSimTest = new ProsumerSim("test", null);
-        ConsumerSimTest.setConsumption(1);
-        assert.equal(ConsumerSimTest.consumer.consumption, 1);
+        let ProsumerSimTest = new ProsumerSim("test", null, null, null, null, null);
+        ProsumerSimTest.prosumer.turbineBreakagePercent = 1;
+        ProsumerSimTest.randomizeTurbineBreaking();
+        assert.equal(ProsumerSimTest.prosumer.production, 0);
+        assert.equal(ProsumerSimTest.prosumer.turbineWorking, false);
+        assert.equal(ProsumerSimTest.prosumer.turbineStatus, "BROKEN! REPAIRMAN CALLED!");
+
     });
 });
 
