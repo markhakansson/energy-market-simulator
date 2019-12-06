37672af86726af192b8445dec865956bbfa1b438
Mark Hakansson <mark.hakansson@live.se>
2019-12-06 15:20:59 +0100

    Fix navbar so that the active item changes on click. Add basic form to profile page

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
     $('#nav-monitor').click(function () {
+        $('a').removeClass('active');
+        $(this).find('a').addClass('active');
         $('#profile').hide();
         $('#monitor').show();
     });
     $('#nav-profile').click(function () {
+        $('a').removeClass('active');
+        $(this).find('a').addClass('active');
         $('#monitor').hide();
         $('#profile').show();
     });
