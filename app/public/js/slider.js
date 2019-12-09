138b6aba98062371f9898f7af98e6d6dd35fdc70
Mark Hakansson <mark.hakansson@live.se>
2019-12-09 13:36:17 +0100

    Update prosumer page and add more scripts

--- app/public/js/slider.js
+++ app/public/js/slider.js
@@ -1,10 +1,10 @@
 function updateSliderVal(id, output) {
     var slider = document.getElementById(id);
-    var op = document.getElementById(output);
-    op.innerHTML = slider.value; // Display the default slider value
+    var out = document.getElementById(output);
+    out.innerHTML = slider.value; // Display the default slider value
 
     // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function () {
-        op.innerHTML = this.value;
+        out.innerHTML = this.value;
     }
 }
--- app/server.js
+++ app/server.js
@@ -4,11 +4,10 @@ var bodyParser = require('body-parser');
 const express_graphql = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./api/schema');
-const jwt = require('jsonwebtoken')
 // const main = require('./sim/controller/main')
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true }); 
-mongoose.connection.on('error', console.log.bind(console, "CONNECTION ERROR!"));
+mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true });
+mongoose.connection.on('error', console.log.bind(console, 'CONNECTION ERROR!'));
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
@@ -23,7 +22,7 @@ var app = express();
 app.use('/graphql', express_graphql({
     schema,
     graphiql: true
-})); 
+}));
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
 /**
  * Authentication
@@ -35,34 +34,34 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/login', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    let data = {
-        "username": username,
-        "password": password
+    const data = {
+        username: username,
+        password: password
     };
 
     mongoose.connection.collection('secrets').find(data, (err, res) => {
-        if(err) throw err;
-        console.log("Logged in!")
+        if (err) throw err;
+        console.log('Logged in!')
     });
 
     return res.redirect('prosumer.html');
 });
 
 app.post('/sign_up', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    let data = {
-        "username": username,
-        "password": password
+    const data = {
+        username: username,
+        password: password
     };
 
     mongoose.connection.collection('secrets').insertOne(data, (err, res) => {
-        if(err) throw err;
-        console.log(res + " inserted!")
+        if (err) throw err;
+        console.log(res + ' inserted!')
     });
 
     return res.redirect('signup_success.html');
@@ -75,27 +74,3 @@ app.get('/', (req, res) => {
     return res.redirect('index.html');
 }).listen(3000, () => console.log('Server listening on port 3000'));
 
-
-
-const signToken = str => {
-    return new Promise(resolve => {
-      resolve(jwt.sign({ apiKey: str }, process.env.JWT_KEY))
-    })
-  };
-  
-  const verifyJwt = req => {
-    let token
-    if (req.query && req.query.hasOwnProperty('access_token')) {
-      token = req.query.access_token
-    } else if (req.headers.authorization && req.headers.authorization.includes('Bearer')) {
-      token = req.headers.authorization.split(' ')[1]
-    }
-    
-    return new Promise((resolve, reject) => {
-      jwt.verify(token, process.env.JWT_KEY, (error, decoded) => {
-        if (error) reject('401: User is not authenticated')
-     
-        resolve(decoded)
-      })
-    })
-  };
