54e2940cd0f75ed058ee5fa8115e69305780bd0b
Aron <arostr-5@student.ltu.se>
2019-12-15 15:10:39 +0100

    grapqhl WILL NOT handle login, sign-up and password update

--- app/api/schemas/user.js
+++ app/api/schemas/user.js
@@ -62,56 +62,6 @@ const UserMutations = {
       user.save();
       return "Image uploaded!";
     }
-  },
-
-  loginUser: {
-    type: GraphQLBoolean,
-    args: {
-      username: { type: new GraphQLNonNull(GraphQLString) },
-      password: { type: new GraphQLNonNull(GraphQLString) }
-    },
-    resolve(parent, args, req) {
-      User.findOne( { username: args.username }, function(err, user) {
-        // Removed to avoid throwing error with possible sensetive data, see OWASP
-        // if (err) throw err;
-
-        if(!user) {
-          throw new Error('Could not find user: ' + args.username);
-        } 
-        user.comparePassword(args.password, function(err, isMatch) { // Avoiding to throw errors here
-          if(!isMatch) throw new Error('Incorrect password for user: ' + args.username);
-
-          if(isMatch) {
-            req.login(user, error => (error ? error : user)); // Passport login
-            console.log(args.username + " is now logged in!");
-            return true;
-          }
-
-        });
-      });
-    }
-  },
-
-  updateUser: {
-    type: GraphQLString,
-    args: {
-      updatePassword: { type: new GraphQLNonNull(GraphQLString) },
-      password: { type: new GraphQLNonNull(GraphQLString) },
-    },
-    async resolve(parent, args, req) {
-      const user = await User.findOne( { username: req.user.username } );
-      if(!user) { 
-        return "user not found!";       
-      }
-      user.comparePassword(args.password, function(err, isMatch) { 
-      if(isMatch) {
-          user.password = args.updatePassword;
-          user.save();
-          return 'Password updated!';
-        }
-        return 'Failed to update password!';;
-      });
-    }
   }
 };
 
