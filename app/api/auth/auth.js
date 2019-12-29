f1b68a7db06ee26785f2cc533bd9dd04c9c01de8
Mark Hakansson <mark.hakansson@live.se>
2019-12-29 15:23:45 +0100

    Format code according to linter rules

--- app/api/auth/auth.js
+++ app/api/auth/auth.js
@@ -1,18 +1,18 @@
-function auth(req, res, next) {
-    if(req.session.user && req.session.cookie) {
+function auth (req, res, next) {
+    if (req.session.user && req.session.cookie) {
         return next();
     }
     return res.redirect('/logout');
 }
-function isManager(req, res, next) {
-    if(req.session.manager) {
+function isManager (req, res, next) {
+    if (req.session.manager) {
         return next();
     }
     return res.redirect('/login');
 }
 
-function isLoggedIn(req, res, next) {
-    if(req.session.user && req.session.cookie) {
+function isLoggedIn (req, res, next) {
+    if (req.session.user && req.session.cookie) {
         return res.redirect('/success');
     }
     return next();
