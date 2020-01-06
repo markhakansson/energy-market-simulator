c2b60147a43d97ca3a1b36e1bf7e16bc41ab928f
Mark Hakansson <mark.hakansson@live.se>
2020-01-06 13:50:42 +0100

    Update simulation tests to work with simulation changes

--- app/sim/tests/market.test.js
+++ app/sim/tests/market.test.js
@@ -4,45 +4,45 @@ const MarketSim = require('../model/market');
 
 describe('#buy0', function () {
     it('should return 0', () => {
-        const marketTest = new MarketSim('test', 2, 1000, 10000);
+        const marketTest = new MarketSim('test', 2, 1000, 10000, 1);
         assert.strictEqual(marketTest.buy(1000), 0);
     });
 });
 
 describe('#sell0', function () {
     it('should return 0', () => {
-        const marketTest = new MarketSim('test', 2, 1000, 0);
+        const marketTest = new MarketSim('test', 2, 1000, 0, 1);
         assert.strictEqual(marketTest.sell(0), 0);
     });
 });
 
 describe('#StartUp', function () {
-    it('status should be true and status is starting up', () => {
-        const marketTest = new MarketSim('test', 2, 10, 10);
+    it('status should be true and status is startup', () => {
+        const marketTest = new MarketSim('test', 2, 10, 10, 1);
         marketTest.generateProduction();
         assert.strictEqual(marketTest.market.startUp, true);
-        assert.strictEqual(marketTest.market.status, 'starting up...');
+        assert.strictEqual(marketTest.market.status, 'startup!');
     });
 });
 
 describe('#Running', function () {
-    it('status should be running and start up false', () => {
-        const marketTest = new MarketSim('test', 2, 10, 10);
-        marketTest.market.startUp = false;
+    it('status should be running', () => {
+        const marketTest = new MarketSim('test', 2, 10, 10, 1);
+        marketTest.market.currBatteryCap = -1;
         marketTest.generateProduction();
         setTimeout(() => {
-            assert.strictEqual(marketTest.market.startUp, false);
-            assert.strictEqual(marketTest.market.status, 'running!');
-        }, 10000);
+            assert.strictEqual(marketTest.status, 'running!');
+            assert.strictEqual(marketTest.market.startUp, true);
+        }, 3000);
     });
 });
 
-describe('#BlackOut', function () {
-    it('status should be blackout!', () => {
-        const marketTest = new MarketSim('test', 2, 10, 10);
-        marketTest.market.currBatteryCap = -1;
+describe('#Stopped', function () {
+    it('status should be stopped and start up false', () => {
+        const marketTest = new MarketSim('test', 2, 10, 10, 1);
+        marketTest.market.startUp = false;
         marketTest.generateProduction();
-        assert.strictEqual(marketTest.market.status, 'BLACK OUT!!!!!');
-        assert.strictEqual(marketTest.market.startUp, true);
+        assert.strictEqual(marketTest.market.startUp, false);
+        assert.strictEqual(marketTest.status, 'stopped!');
     });
 });
