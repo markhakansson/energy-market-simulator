1f03be2a6cce7fb74685ff3d1e0ee1a57b3a4ba9
Mark Hakansson <mark.hakansson@live.se>
2020-01-07 14:40:48 +0100

    Ignore illegal and bad values in buy, sell in market sim

--- app/sim/model/market.js
+++ app/sim/model/market.js
@@ -57,7 +57,7 @@ class MarketSim {
             //     self.market = doc;
             // }
             if (err) throw err;
-            if(doc) {
+            if (doc) {
                 self.market = doc;
             } else {
                 self.market.save();
@@ -79,28 +79,28 @@ class MarketSim {
         const self = this.market;
 
         if (demand < 0 || demand == null) {
-            Logger.warn(
+            Logger.error(
                 'In market [' + self.name + '] when receiving a buy order, expected postive Number. Recieved: ' +
                 demand + '.'
             );
-        }
-
-        this.demand += demand;
-
-        // Needs to reset the demand for each tick.
-        setTimeout(() => {
-            this.demand -= demand;
-        }, 1.05 * this.timeMultiplier);
-
-        // Use market output if possible, else try to use the battery
-        let usableEnergy = this.marketOutput - demand;
-        if (usableEnergy >= 0) {
-            this.marketOutput -= demand;
-            return demand;
         } else {
-            usableEnergy = this.marketOutput + this.useBattery(demand - this.marketOutput);
-            this.marketOutput = 0;
-            return usableEnergy;
+            this.demand += demand;
+
+            // Needs to reset the demand for each tick.
+            setTimeout(() => {
+                this.demand -= demand;
+            }, 1.05 * this.timeMultiplier);
+
+            // Use market output if possible, else try to use the battery
+            let usableEnergy = this.marketOutput - demand;
+            if (usableEnergy >= 0) {
+                this.marketOutput -= demand;
+                return demand;
+            } else {
+                usableEnergy = this.marketOutput + this.useBattery(demand - this.marketOutput);
+                this.marketOutput = 0;
+                return usableEnergy;
+            }
         }
     }
 
@@ -112,22 +112,21 @@ class MarketSim {
         const self = this.market;
 
         if (demand < 0 || demand == null) {
-            Logger.warn(
+            Logger.error(
                 'In market [' + self.name + '] when receiving a sell order, expected postive Number. Recieved: ' +
                 demand + '.'
             );
+        } else {
+            this.demand -= demand;
+
+            this.chargeBattery(self.fillBatteryRatio * demand);
+            this.marketOutput += ((1 - self.fillBatteryRatio) * demand);
+
+            // Needs to reset the demand for each tick.
+            setTimeout(() => {
+                this.demand += demand;
+            }, 1.05 * this.timeMultiplier);
         }
-
-        this.demand -= demand;
-
-        this.chargeBattery(self.fillBatteryRatio * demand);
-        this.marketOutput += ((1 - self.fillBatteryRatio) * demand);
-
-        // Needs to reset the demand for each tick.
-        setTimeout(() => {
-            this.demand += demand;
-        }, 1.05 * this.timeMultiplier);
-
         return 0;
     }
 
@@ -226,7 +225,6 @@ class MarketSim {
                 this.plantInOperation = true;
                 this.startupInitiated = false;
             }, 2 * this.timeMultiplier);
-
         }
     }
 
