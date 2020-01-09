c1467a543dbe5019345b2f68429636989246b4ad
Mark Hakansson <mark.hakansson@live.se>
2020-01-09 15:31:37 +0100

    Update prosumer and weather db models with more validation checks

--- app/db/model/weather.js
+++ app/db/model/weather.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 const weather = new Schema({
     name: { type: String, required: true },
     timestamp: { type: Date, default: Date.now() },
-    wind_speed: { type: Number, default: 4.0 },
+    wind_speed: {
+        type: Number,
+        default: 4.0,
+        min: 0.0
+    },
     temperature: { type: Number, default: 20.0 },
     market: { type: String, required: true }
 });
